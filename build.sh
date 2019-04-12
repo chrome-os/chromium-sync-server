5dad26b1aa64fc1523d5ac5d25822d9fc2d1d3c2
zsp <zsp@ur.city>
2019-04-12 15:38:42 +0800

    master

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@ sudo apt-get -y install \
     libgconf-2-4 \
     libnss3
 
-mkdir -p ~/workdir
-cd ~/workdir
+mkdir -p ~~/workdir
+cd ~~/workdir
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git --depth=1
 
-PATH=$PATH:~/workdir/depot_tools
+PATH=$PATH:~~/workdir/depot_tools
 
 mkdir -p chromium
 cd chromium
@@ -55,18 +55,18 @@ gn gen --enable_nacl=false --symbol_level=0 --remove_webcore_debug_symbols=true 
     autoninja -C out/Default third_party/protobuf:py_proto && \
     autoninja -C out/Default components/sync/protocol:protocol
 	
-mkdir -p ~/workdir/sync-server
-cd ~/workdir/sync-server
+mkdir -p ~~/workdir/sync-server
+cd ~~/workdir/sync-server
 
 # Import require packages and fix imports for a standalone run
 mkdir sync lib && \
-    cp -rp /workdir/chromium/src/out/Default/pyproto/components/sync/protocol/ lib/protocol/ && \
-    cp -rp /workdir/chromium/src/out/Default/pyproto/google . && \
-    cp -rp /workdir/chromium/src/components/sync/tools/testserver/ sync/testserver/ && \
-    cp -rp /workdir/chromium/src/net/tools/testserver/ lib/testserver/ && \
-    cp -rp /workdir/chromium/src/third_party/tlslite/tlslite/ . && \
-	rm -rf /workdir/chromium && \
-	rm -rf /workdir/depot_tools && \
+    cp -rp ~/workdir/chromium/src/out/Default/pyproto/components/sync/protocol/ lib/protocol/ && \
+    cp -rp ~/workdir/chromium/src/out/Default/pyproto/google . && \
+    cp -rp ~/workdir/chromium/src/components/sync/tools/testserver/ sync/testserver/ && \
+    cp -rp ~/workdir/chromium/src/net/tools/testserver/ lib/testserver/ && \
+    cp -rp ~/workdir/chromium/src/third_party/tlslite/tlslite/ . && \
+	rm -rf ~/workdir/chromium && \
+	rm -rf ~/workdir/depot_tools && \
     touch lib/testserver/__init__.py && \
 	touch lib/protocol/__init__.py && \
 	touch sync/testserver/__init__.py && \
@@ -74,4 +74,4 @@ mkdir sync lib && \
     touch lib/__init__.py && \
 	sed -i 's#import echo_message#from lib.testserver import echo_message#' sync/testserver/sync_testserver.py && \
     sed -i 's#import testserver_base#from lib.testserver import testserver_base#' sync/testserver/sync_testserver.py && \
-    /bin/bash -c 'for line in $(find /workdir/sync-server/lib/protocol/ -name "*.py" -type f -exec basename {} .py \;); do sed -i "s/import $line/from lib.protocol import $line/" /workdir/sync-server/sync/testserver/chromiumsync.py; done'
+    /bin/bash -c 'for line in $(find ~/workdir/sync-server/lib/protocol/ -name "*.py" -type f -exec basename {} .py \;); do sed -i "s/import $line/from lib.protocol import $line/" ~/workdir/sync-server/sync/testserver/chromiumsync.py; done'
--- cp.sh
+++ cp.sh
@@ -1,16 +1,16 @@
 	
-mkdir -p ~/workdir/sync-server
-cd ~/workdir/sync-server
+mkdir -p ~~/workdir/sync-server
+cd ~~/workdir/sync-server
 
 # Import require packages and fix imports for a standalone run
 mkdir sync lib && \
-    cp -rp /workdir/chromium/src/out/Default/pyproto/components/sync/protocol/ lib/protocol/ && \
-    cp -rp /workdir/chromium/src/out/Default/pyproto/google . && \
-    cp -rp /workdir/chromium/src/components/sync/tools/testserver/ sync/testserver/ && \
-    cp -rp /workdir/chromium/src/net/tools/testserver/ lib/testserver/ && \
-    cp -rp /workdir/chromium/src/third_party/tlslite/tlslite/ . && \
-	rm -rf /workdir/chromium && \
-	rm -rf /workdir/depot_tools && \
+    cp -rp ~/workdir/chromium/src/out/Default/pyproto/components/sync/protocol/ lib/protocol/ && \
+    cp -rp ~/workdir/chromium/src/out/Default/pyproto/google . && \
+    cp -rp ~/workdir/chromium/src/components/sync/tools/testserver/ sync/testserver/ && \
+    cp -rp ~/workdir/chromium/src/net/tools/testserver/ lib/testserver/ && \
+    cp -rp ~/workdir/chromium/src/third_party/tlslite/tlslite/ . && \
+	rm -rf ~/workdir/chromium && \
+	rm -rf ~/workdir/depot_tools && \
     touch lib/testserver/__init__.py && \
 	touch lib/protocol/__init__.py && \
 	touch sync/testserver/__init__.py && \
@@ -18,4 +18,4 @@ mkdir sync lib && \
     touch lib/__init__.py && \
 	sed -i 's#import echo_message#from lib.testserver import echo_message#' sync/testserver/sync_testserver.py && \
     sed -i 's#import testserver_base#from lib.testserver import testserver_base#' sync/testserver/sync_testserver.py && \
-    /bin/bash -c 'for line in $(find /workdir/sync-server/lib/protocol/ -name "*.py" -type f -exec basename {} .py \;); do sed -i "s/import $line/from lib.protocol import $line/" /workdir/sync-server/sync/testserver/chromiumsync.py; done'
+    /bin/bash -c 'for line in $(find ~/workdir/sync-server/lib/protocol/ -name "*.py" -type f -exec basename {} .py \;); do sed -i "s/import $line/from lib.protocol import $line/" ~/workdir/sync-server/sync/testserver/chromiumsync.py; done'
